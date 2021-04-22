acf6a1d10935d88db5ae255b47f6df9aa91f375f
Dustin Martinez <martinezdustin736@gmail.com>
2021-04-22 01:34:27 -0500

    added 1-5

--- code.js
+++ code.js
@@ -2,7 +2,29 @@
 function helloWorld() {
     return "Hello, World!";
 }
-
 function sayHello(){
+    return "hello"
+}
+function sayHello(){
+return "Hello, Jane!";
+}
 
+
+
+function sayHello(input){
+    if(input === "Alex"){
+        return "Hello, Alex!"
+    }
+    else {
+        return "Hello, Jane!"
+    }
+}
+
+function sayHello (input){
+    if (input === "Pat"){
+        return "Hello, Pat!"
+    }
+    else {
+        return "Hello, Alex!"
+    }
 }
